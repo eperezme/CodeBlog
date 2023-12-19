fb0121ef4e27b253e818343156993a0a9c61cf96
Viper <eduardperezmendez@gmail.com>
2023-12-19 14:12:44 +0100

    Update config in hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,15 @@
 // Add this to your /api/hello.js file
-export const config = { runtime: "edge" };
+export const config = {
+	api: {
+		externalResolver: true,
+		bodyParser: false,
+	},
+	pages: {
+		edge: {
+			maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+		},
+	},
+};
 
 // Your existing API handler
 export default function handler(req, res) {
