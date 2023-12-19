28cd8d60bb3f7e009074591785ff733152e3af02
Viper <eduardperezmendez@gmail.com>
2023-12-19 14:38:25 +0100

    Update API config to use experimental-edge runtime

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,18 +1,5 @@
 // Add this to your /api/hello.js file
-export const config = {
-	api: {
-		externalResolver: true,
-		bodyParser: false,
-	},
-	pages: {
-		edge: {
-			maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-			experimental: {
-				edge: true,
-			},
-		},
-	},
-};
+export const config = { runtime: "experimental-edge" };
 
 // Your existing API handler
 export default function handler(req, res) {
